6f972d9e8504e5548b1d6afe07611f3c092233e6
Ben Bressette <bressebe@gmail.com>
2019-06-17 16:26:38 -0400

    init

---
+++ MainForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace CobolDataTypeConverter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRawPic = new System.Windows.Forms.TextBox();
+            this.txtOutPic = new System.Windows.Forms.TextBox();
+            this.txtRawValue = new System.Windows.Forms.TextBox();
+            this.txtOutValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnNewQuestion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtRawPic
+            // 
+            this.txtRawPic.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.txtRawPic.Location = new System.Drawing.Point(243, 66);
+            this.txtRawPic.Name = "txtRawPic";
+            this.txtRawPic.ReadOnly = true;
+            this.txtRawPic.Size = new System.Drawing.Size(203, 34);
+            this.txtRawPic.TabIndex = 0;
+            // 
+            // txtOutPic
+            // 
+            this.txtOutPic.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.txtOutPic.Location = new System.Drawing.Point(243, 110);
+            this.txtOutPic.Name = "txtOutPic";
+            this.txtOutPic.ReadOnly = true;
+            this.txtOutPic.Size = new System.Drawing.Size(203, 34);
+            this.txtOutPic.TabIndex = 1;
+            // 
+            // txtRawValue
+            // 
+            this.txtRawValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.txtRawValue.Location = new System.Drawing.Point(577, 66);
+            this.txtRawValue.Name = "txtRawValue";
+            this.txtRawValue.ReadOnly = true;
+            this.txtRawValue.Size = new System.Drawing.Size(203, 34);
+            this.txtRawValue.TabIndex = 2;
+            // 
+            // txtOutValue
+            // 
+            this.txtOutValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.txtOutValue.Location = new System.Drawing.Point(577, 110);
+            this.txtOutValue.Name = "txtOutValue";
+            this.txtOutValue.Size = new System.Drawing.Size(203, 34);
+            this.txtOutValue.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(634, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Record the result if RAW-DATA was moved to OUT-DATA:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(215, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "01 RAW-DATA PIC";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label3.Location = new System.Drawing.Point(470, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "VALUE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label4.Location = new System.Drawing.Point(786, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = ".";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label5.Location = new System.Drawing.Point(14, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(213, 29);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "01 OUT-DATA PIC";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label6.Location = new System.Drawing.Point(470, 115);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 29);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "VALUE";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label7.Location = new System.Drawing.Point(786, 117);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 29);
+            this.label7.TabIndex = 10;
+            this.label7.Text = ".";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnSubmit.Location = new System.Drawing.Point(577, 159);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(203, 43);
+            this.btnSubmit.TabIndex = 11;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnNewQuestion
+            // 
+            this.btnNewQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnNewQuestion.Location = new System.Drawing.Point(17, 160);
+            this.btnNewQuestion.Name = "btnNewQuestion";
+            this.btnNewQuestion.Size = new System.Drawing.Size(429, 42);
+            this.btnNewQuestion.TabIndex = 12;
+            this.btnNewQuestion.Text = "Generate New Question";
+            this.btnNewQuestion.UseVisualStyleBackColor = true;
+            this.btnNewQuestion.Click += new System.EventHandler(this.btnNewQuestion_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 227);
+            this.Controls.Add(this.btnNewQuestion);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutValue);
+            this.Controls.Add(this.txtRawValue);
+            this.Controls.Add(this.txtOutPic);
+            this.Controls.Add(this.txtRawPic);
+            this.Name = "MainForm";
+            this.Text = "Cobol Data Type Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRawPic;
+        private System.Windows.Forms.TextBox txtOutPic;
+        private System.Windows.Forms.TextBox txtRawValue;
+        private System.Windows.Forms.TextBox txtOutValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnNewQuestion;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CobolDataTypeConverter
+{
+    public partial class MainForm : Form
+    {
+        private StringBuilder rawNumber = new StringBuilder();
+        private StringBuilder rawPic = new StringBuilder();
+        private StringBuilder outPic = new StringBuilder();
+        private StringBuilder outNumber = new StringBuilder();
+        private Random random = new Random();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            displayQuestion();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            DialogResult prompt;
+
+            if (txtOutValue.Text == outNumber.ToString())
+            {
+                prompt = MessageBox.Show("Correct! Press OK to generate a new question.");
+                displayQuestion();
+            }
+            else
+            {
+                prompt = MessageBox.Show("Incorrect. Try again?", "", MessageBoxButtons.YesNo);
+                if (prompt == DialogResult.No)
+                {
+                    prompt = MessageBox.Show(outNumber + " was the answer. Press OK to generate a new question.");
+                    displayQuestion();
+                }
+            }
+        }
+
+        private void displayQuestion()
+        {
+            generateQuestion();
+
+            txtRawValue.Text = rawNumber.ToString();
+            txtRawPic.Text = rawPic.ToString();
+            txtOutPic.Text = outPic.ToString();
+            txtOutValue.Text = "";
+        }
+
+        private void generateQuestion()
+        {
+            rawNumber.Clear();
+            rawPic.Clear();
+            outPic.Clear();
+            outNumber.Clear();
+            
+            int rawLength = random.Next(2, 10);
+            int rawDecimalIndex = random.Next(1, rawLength + 1);
+
+            rawNumber.Append(random.Next((int)Math.Pow(10, rawLength - 1), (int)Math.Pow(10, rawLength) - 1));
+            if (rawLength != rawDecimalIndex)
+                rawNumber.Insert(rawDecimalIndex, ".");
+
+            int outLength = random.Next(2, 10);
+            int outDecimalIndex = random.Next(1, outLength + 1);
+            string outPicChar = "9";
+            string outValueChar = "0";
+
+            bool usesS = false, usesCR = false, isNegative = false, usesCommas = false, isZero = false;
+
+            if (random.Next(0, 2) == 1)
+            {
+                usesS = true;
+                if (random.Next(0, 2) == 0)
+                    isNegative = true;
+            }
+
+            if (random.Next(0, 2) == 1)
+                usesCommas = true;
+
+            if (rawDecimalIndex < outDecimalIndex)
+            {
+                switch (random.Next(0, 6))
+                {
+                    case 0:
+                        outPicChar = "Z";
+                        outValueChar = "b";
+                        break;
+                    case 1:
+                        outPicChar = "$";
+                        outValueChar = "b";
+                        break;
+                    case 2:
+                        outPicChar = "*";
+                        outValueChar = "*";
+                        break;
+                    case 3:
+                        outPicChar = "+";
+                        outValueChar = "b";
+                        break;
+                    case 4:
+                        outPicChar = "-";
+                        outValueChar = "b";
+                        break;
+                }
+            }
+
+            if (outPicChar != "+" && outPicChar != "-" && usesS && random.Next(0, 2) == 1)
+                usesCR = true;
+
+            // raw pic init
+            if (rawLength - (rawLength - rawDecimalIndex) > 3)
+                rawPic.Append("9(").Append(rawLength - (rawLength - rawDecimalIndex)).Append(")");
+            else
+                for (int ctr = rawLength - (rawLength - rawDecimalIndex); ctr > 0; ctr--)
+                    rawPic.Append("9");
+            if (rawLength != rawDecimalIndex)
+                rawPic.Append("V");
+            if (rawLength - rawDecimalIndex > 3)
+                rawPic.Append("9(").Append(rawLength - rawDecimalIndex).Append(")");
+            else
+                for (int ctr = rawLength - rawDecimalIndex; ctr > 0; ctr--)
+                    rawPic.Append("9");
+
+            // out pic init
+            if (outLength - (outLength - outDecimalIndex) > 3 && !usesCommas)
+                outPic.Append(outPicChar).Append("(").Append(outLength - (outLength - outDecimalIndex)).Append(")");
+            else
+            {
+                for (int ctr = outLength - (outLength - outDecimalIndex); ctr > 0; ctr--)
+                {
+                    outPic.Append(outPicChar);
+                    if (ctr == 4 || ctr == 7)
+                        outPic.Append(",");
+                }
+            }
+            if (outLength != outDecimalIndex)
+                outPic.Append(".");
+            if (outLength - outDecimalIndex > 3)
+                outPic.Append("9(").Append(outLength - outDecimalIndex).Append(")");
+            else
+                for (int ctr = outLength - outDecimalIndex; ctr > 0; ctr--)
+                    outPic.Append("9");
+
+            // outNumber init
+            outNumber.Append(rawNumber);
+            if (rawDecimalIndex > outDecimalIndex)
+            {
+                outNumber.Remove(0, rawDecimalIndex - outDecimalIndex);
+                if (Convert.ToDouble(outNumber.ToString()) == 0.0)
+                    isZero = true;
+            }
+            else if (rawDecimalIndex < outDecimalIndex)
+            {
+                bool first = true;
+                for (int ctr = outDecimalIndex - rawDecimalIndex; ctr > 0; ctr--)
+                {
+                    if (first && outPicChar != "9" && outPicChar != "Z")
+                    {
+                        switch (outPicChar)
+                        {
+                            case "+":
+                                outNumber.Insert(0, isNegative ? "-" : outPicChar);
+                                break;
+                            case "-":
+                                outNumber.Insert(0, isNegative ? "-" : "b");
+                                break;
+                            default:
+                                outNumber.Insert(0, outPicChar);
+                                break;
+                        }
+                        first = false;
+                    }
+                    else
+                        outNumber.Insert(0, outValueChar);
+                }
+            }
+            if (rawLength - rawDecimalIndex > outLength - outDecimalIndex)
+            {
+                outNumber.Remove(outNumber.Length - ((rawLength - rawDecimalIndex) - (outLength - outDecimalIndex)), ((rawLength - rawDecimalIndex) - (outLength - outDecimalIndex)));
+                if (outLength - outDecimalIndex == 0)
+                    outNumber.Remove(outNumber.Length - 1, 1);
+                if (rawDecimalIndex > outDecimalIndex && Convert.ToDouble(outNumber.ToString()) == 0.0)
+                    isZero = true;
+            }
+            else if (rawLength - rawDecimalIndex < outLength - outDecimalIndex)
+            {
+                if (rawLength - rawDecimalIndex == 0)
+                    outNumber.Insert(outNumber.Length, ".");
+                for (int ctr = (outLength - outDecimalIndex) - (rawLength - rawDecimalIndex); ctr > 0; ctr--)
+                    outNumber.Insert(outNumber.Length, "0");
+            }
+            if (usesCommas)
+            {
+                if (outDecimalIndex > 3)
+                {
+                    if (outDecimalIndex - 3 > outDecimalIndex - rawDecimalIndex || outValueChar == "0")
+                        outNumber.Insert(outDecimalIndex - 3, ",");
+                    else
+                        outNumber.Insert(outDecimalIndex - 4, outValueChar);
+                }
+                if (outDecimalIndex > 6)
+                {
+                    if (outDecimalIndex - 6 > outDecimalIndex - rawDecimalIndex || outValueChar == "0")
+                        outNumber.Insert(outDecimalIndex - 6, ",");
+                    else
+                        outNumber.Insert(outDecimalIndex - 7, outValueChar);
+                }
+            }
+
+            // signs for raw pic, value
+            if (usesS)
+                rawPic.Insert(0, "S");
+            if (isNegative)
+                rawNumber.Insert(0, "-");
+
+            // db, cr labels if needed
+            if (usesCR)
+            {
+                string sign = random.Next(0, 2) == 1 ? "DB" : "CR";
+                outPic.Append(sign);
+                outNumber.Append(isNegative && !isZero ? sign : "bb");
+            }
+        }
+
+        private void btnNewQuestion_Click(object sender, EventArgs e)
+        {
+            displayQuestion();
+        }
+    }
+}
